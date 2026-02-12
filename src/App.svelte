7f5df55eda9dbe16897655c5f6283e158fe35691
sebastienfernandez <sebastienfernandez@protonmail.com>
2026-02-12 22:15:51 +0100

    add two titles

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
-  
+  let dashboard = "Menu interactif";
+  let todolist = "Liste de tâches";
 </script>
 
 <main>
   <h1>Biohacking personnel</h1>
-
-  
+  <div>
+    <h2>{dashboard.toLocaleUpperCase()}</h2>
+  </div>
+  <div>
+    <h2>{todolist.toLocaleUpperCase()}</h2>
+  </div>
 </main>
 
 <style>
