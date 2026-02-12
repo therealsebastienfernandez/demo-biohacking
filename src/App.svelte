a3c8a9619be3e697e28ab9a6ddf56fb12761b2ab
sebastienfernandez <sebastienfernandez@protonmail.com>
2026-02-12 23:09:26 +0100

    tuto svelte : intro + state

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import Task from "./lib/Task.svelte";
+  import Counter from "./lib/Counter.svelte";
+
   let dashboard = "Menu interactif";
   let todolist = "Liste de tâches";
 </script>
@@ -7,13 +10,18 @@
   <h1>Biohacking personnel</h1>
   <div>
     <h2>{dashboard.toLocaleUpperCase()}</h2>
+    <p>Vous pouvez dans le {dashboard} voir vos statistiques</p>
+    <Counter />
   </div>
   <div>
     <h2>{todolist.toLocaleUpperCase()}</h2>
+    <Task />
   </div>
 </main>
 
 <style>
-  
+  h1 {
+    color: cyan;
+  }
   
 </style>
---
+++ src/lib/Task.svelte
@@ -0,0 +1,5 @@
+<script>
+ let important = "Elle est <strong>importante</strong>"
+</script>
+<p>Ceci est ma tâche</p>
+<p>{@html important}</p>
